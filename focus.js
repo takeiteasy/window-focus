6f3bf2314d9a22674562719e138008de6a2d64f9
Rusty Shackleford <gigolo@hotmail.co.uk>
2016-01-26 00:19:40 +0000

    take screenshot on start and delete on quit

--- focus.js
+++ focus.js
@@ -1,38 +1,34 @@
 'use strict';
 
-const electron = require('electron');
-const app = electron.app;
+const bg_loc = '/tmp/tmp_bg.png';
+const cp     = require('child_process');
+const screen = cp.spawnSync('screencapture', ['-x', bg_loc]);
+
+const electron      = require('electron');
+const app           = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-var mainWindow = null;
+var   mainWindow    = null;
 
 app.on('window-all-closed', function() {
   app.quit();
 });
 
 app.on('ready', function() {
-  var atomScreen = require('screen');
-  var displays = atomScreen.getAllDisplays();
-  var externalDisplay = null;
-  for (var i in displays) {
-    if (displays[i].bounds.x > 0 || displays[i].bounds.y > 0) {
-      externalDisplay = displays[i];
-      break;
-    }
-  }
+  mainWindow = new BrowserWindow({
+    width                  : 3840,
+    height                 : 2160,
+    transparent            : true,
+    frame                  : false,
+    moveable               : false,
+    resizable              : false,
+    enableLargerThanScreen : true,
+    x                      : -8,
+    y                      : -8
+  });
 
-    mainWindow = new BrowserWindow({
-      width                  : 3840,
-      height                 : 2160,
-      transparent            : true,
-      frame                  : false,
-      moveable               : false,
-      resizable              : false,
-      enableLargerThanScreen : true,
-      x: -8,
-      y: -8
-    });
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
-    mainWindow.on('closed', function() {
-      mainWindow = null;
-    });
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.on('closed', function() {
+    require('fs').unlink(bg_loc);
+    mainWindow = null;
+  });
 });
