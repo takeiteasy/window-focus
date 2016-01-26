428f2b741b9049c6be4cf4d4e798b30ee777efd4
Rusty Shackleford <gigolo@hotmail.co.uk>
2016-01-26 01:01:29 +0000

    auto size

--- focus.js
+++ focus.js
@@ -1,8 +1,11 @@
 'use strict';
 
-const bg_loc = '/tmp/tmp_bg.png';
-const cp     = require('child_process');
-const screen = cp.spawnSync('screencapture', ['-x', bg_loc]);
+const bg_loc  = '/tmp/tmp_bg.png';
+const cp      = require('child_process');
+const screenp = cp.spawnSync('screencapture', ['-x', bg_loc]);
+
+const sizep  = cp.spawnSync('sh', ['-c', 'system_profiler SPDisplaysDataType | grep -Eoh "[0-9]{4} x [0-9]{4}" | tr -d [:space:]' ]);
+var size_tmp = sizep.stdout.toString().split('x');
 
 const electron      = require('electron');
 const app           = electron.app;
@@ -15,8 +18,8 @@ app.on('window-all-closed', function() {
 
 app.on('ready', function() {
   mainWindow = new BrowserWindow({
-    width                  : 3840,
-    height                 : 2160,
+    width                  : parseInt(size_tmp[0]),
+    height                 : parseInt(size_tmp[1]),
     transparent            : true,
     frame                  : false,
     moveable               : false,
@@ -25,8 +28,8 @@ app.on('ready', function() {
     x                      : -8,
     y                      : -8
   });
-
   mainWindow.loadURL('file://' + __dirname + '/index.html');
+
   mainWindow.on('closed', function() {
     require('fs').unlink(bg_loc);
     mainWindow = null;
