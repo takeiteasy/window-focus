936098ed221bc3f835bd67a2cb034023cbe7f6b3
Rusty Shackleford <gigolo@hotmail.co.uk>
2016-01-26 01:43:27 +0000

    finished, quits on lost focus

--- focus.js
+++ focus.js
@@ -4,8 +4,8 @@ const bg_loc  = '/tmp/tmp_bg.png';
 const cp      = require('child_process');
 const screenp = cp.spawnSync('screencapture', ['-x', bg_loc]);
 
-const sizep  = cp.spawnSync('sh', ['-c', 'system_profiler SPDisplaysDataType | grep -Eoh "[0-9]{4} x [0-9]{4}" | tr -d [:space:]' ]);
-var size_tmp = sizep.stdout.toString().split('x');
+const sizep    = cp.spawnSync('sh', ['-c', 'system_profiler SPDisplaysDataType | grep -Eoh "[0-9]{4} x [0-9]{4}" | tr -d [:space:]' ]);
+var   size_tmp = sizep.stdout.toString().split('x');
 
 const electron      = require('electron');
 const app           = electron.app;
@@ -29,6 +29,19 @@ app.on('ready', function() {
     y                      : -8
   });
   mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  const setcp = cp.spawnSync('osascript', ['-e', 'tell application "' + process.argv[2] + '" to activate']);
+  if (setcp.stderr.toString().length > 1) {
+    console.log("ERROR: \"" + process.argv[2] + "\" is not a valid running window!");
+    app.quit();
+  }
+  setInterval(function() {
+    const x = cp.spawnSync('sh', ["./activewin.sh"]);
+    if (x.stderr.toString().slice(0, -1).toLowerCase() != process.argv[2].toLowerCase()) {
+      console.log("Lost focus of \"" + process.argv[2] + "\"! Bye!");
+      app.quit();
+    }
+  }, 1);
 
   mainWindow.on('closed', function() {
     require('fs').unlink(bg_loc);
